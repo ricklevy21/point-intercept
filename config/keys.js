144a51402145abad3a5d028902afff160547ab6e
Rick Levy <ricklevy21@gmail.com>
2020-09-03 14:44:17 -0600

    made safe

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://ricklevy21:thWuIzoA1Vjog3fv@cluster0.bcip7.mongodb.net/point-intercept?retryWrites=true&w=majority',
-    secretOrKey: "schmaltz"
-  };
