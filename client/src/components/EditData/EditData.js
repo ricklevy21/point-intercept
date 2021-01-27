076d0baa632e161198c5c266078821fe29085edb
Rick Levy <ricklevy21@gmail.com>
2021-01-27 11:37:43 -0700

    eit data page displays project title

---
+++ client/src/components/EditData/EditData.js
@@ -0,0 +1,39 @@
+//dependencies
+import React, { useState, useEffect } from "react";
+import {useHistory, useParams } from 'react-router-dom'
+import API from "../../utils/API";
+import ResumeProjectName from "../AddTransect/ResumeProjectName"
+
+const EditProjectData = () => {
+
+    //get _id param (transectID) so that it can be accessed to for displaying data and for adding transect name
+    const { _id } = useParams()
+
+    const history = useHistory()
+
+    //setting component's initial state
+    //hook for state where project title is displayed
+    const [project, setProject] =useState([])
+
+    //display the project title once the component mounts
+    useEffect(() => {
+        //GET Method for pulling project name
+        API.getProjectByID(_id)
+        .then(res => {
+            setProject(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+    
+
+    return (
+        <>
+            <ResumeProjectName
+                id={project._id}
+                project={project.project}
+            />
+        </>
+    )
+}
+
+export default EditProjectData
--- client/src/pages/Edit.js
+++ client/src/pages/Edit.js
@@ -1,10 +1,16 @@
 //dependencies
 import React from 'react'
+import EditProjectData from '../components/EditData/EditData'
 
 
 const Edit = () => {
     return (
         <>
+            <div className="row">
+                <div className="col s12">
+                    <EditProjectData/>
+                </div>
+            </div>
         </>
     )
 }
