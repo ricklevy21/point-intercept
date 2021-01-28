1e54539d2e898df805bf7ad9560a6063fb7e01ac
Rick Levy <ricklevy21@gmail.com>
2021-01-28 12:20:51 -0700

    table rendering improvements

--- client/src/components/EditData/Table.js
+++ client/src/components/EditData/Table.js
@@ -4,6 +4,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import SearchIcon from "@material-ui/icons/Search";
 import SaveIcon from "@material-ui/icons/Save";
 import { Button } from "@material-ui/core";
+import moment from 'moment'
 
 const Table = (props) => {
     console.log(props)
@@ -11,22 +12,70 @@ const Table = (props) => {
         <div style={{ maxWidth: '100%' }}>
         <MaterialTable
           columns={[
-            { title: 'transect', field: 'transect' },
-            { title: 'date', field: 'date' },
-            { title: 'latitude', field: 'latitude'},
-            { title: 'longitude', field: 'longitude'},
-            { title: 'elevation', field: 'elevation'},
-            { title: 'crew', field: 'crew'},
-            { title: 'additionalSpecies', field: 'additionalSpecies'},
-            { title: 'point', field: 'point'},
-            { title: 'ground_surface', field: 'ground_surface'},
-            { title: 'soil_moisture_percentage', field: 'soil_moisture_percentage'},
-            { title: 'shrub_density_detail', field: 'shrub_density_detail'},
-            { title: 'canopy_score', field: 'canopy_score'},
-            { title: 'canopy_taxa', field: 'canopy_taxa'},
-            { title: 'hit_one', field: 'hit_one'},
-            { title: 'hit_two', field: 'hit_two'}
-
+            {
+                title: 'date',
+                field: 'date',
+                render: rowData => moment(rowData.validated_at).format('MM/DD/YYYY')
+            },
+            {
+                title: 'transect',
+                field: 'transect'
+            },
+            {
+                title: 'point',
+                field: 'point'
+            },
+            {
+                title: 'hit_one',
+                field: 'hit_one'
+            },
+            {
+                title: 'hit_two',
+                field: 'hit_two',
+                render: rowData => rowData.hit_two ? rowData.hit_two.join(', '): 'nooo'
+            },
+            {
+                title: 'ground_surface',
+                field: 'ground_surface'
+            },
+            {
+                title: 'soil_moisture_percentage',
+                field: 'soil_moisture_percentage'
+            },
+            {
+                title: 'canopy_score',
+                field: 'canopy_score'
+            },
+            {
+                title: 'canopy_taxa',
+                field: 'canopy_taxa',
+                render: rowData => rowData.canopy_taxa ? rowData.canopy_taxa.join(', '): 'nooo'
+            },
+            {
+                title: 'shrub_density_detail',
+                field: 'shrub_density_detail'
+            },
+            {
+                title: 'additionalSpecies',
+                field: 'additionalSpecies',
+                render: rowData => rowData.additionalSpecies ? rowData.additionalSpecies.join(', '): 'nooo'
+            },
+            {
+                title: 'latitude',
+                field: 'latitude'
+            },
+            {
+                title: 'longitude',
+                field: 'longitude'
+            },
+            {
+                title: 'elevation',
+                field: 'elevation'
+            },
+            {
+                title: 'crew',
+                field: 'crew'
+            }
           ]}
             data={props.data}
             title={props.project}
