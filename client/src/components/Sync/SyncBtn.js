e817ac42a703b213f127347f41f187adfe6c348b
Rick Levy <Richard.Levy@BotanicGardens.org>
2021-03-03 15:48:14 -0700

    sync page and button added

---
+++ client/src/components/Sync/SyncBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SyncBtn = () => {
+    return (
+        <>
+        <button
+        style={{ float: "right", marginBottom: 10 }} className="btn btn-lg btn-dark btn-block"
+        >sync offline data
+        </button>
+        </>
+    )
+}
+export default SyncBtn
---
+++ client/src/pages/Sync.js
@@ -0,0 +1,20 @@
+//dependencies
+import React from 'react'
+import SyncBtn from '../components/Sync/SyncBtn'
+
+
+const Sync = () => {
+    //open indexedDB to create stores fro offline data
+    const request = window.indexedDB.open("point-intercept", 1);
+    //create schema for indexedDB
+
+    return (
+        <>
+        <h6>Data recorded while offline is cached in the browser. Once back online, click the button below to send data to the databse and clear the browser's cache.</h6>
+        <br></br>
+        <SyncBtn />
+        </>
+    )
+}
+
+export default Sync
