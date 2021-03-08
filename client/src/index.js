5abe2eff60746924aad8828b4b592206ad7837b3
Rick Levy <Richard.Levy@BotanicGardens.org>
2021-03-08 14:28:16 -0700

    index.js fix

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <>
     <Auth0Provider
       domain="process.env.REACT_APP_AUTH0_DOMAIN"
-      clientId="REACT_APP_AUTH0_CLIENT_ID"
+      clientId="process.env.REACT_APP_AUTH0_CLIENT_ID"
       redirectUri={window.location.origin}
       >
         <App />
